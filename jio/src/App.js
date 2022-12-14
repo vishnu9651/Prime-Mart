ac315ccf5086651323af10320ee908a0799df159
sumeetmore1997 <101966263+sumeetmore1997@users.noreply.github.com>
2022-12-15 00:22:22 +0530

    product

--- jio/src/App.js
+++ jio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import MainRoutes from "./Pages/MainRoutes";
+import Product from "./Pages/Product";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <MainRoutes />
+      {/* <Navbar />
+      <MainRoutes /> */}
+      <Product/>
     </div>
   );
 }
---
+++ jio/src/Pages/Product.jsx
@@ -0,0 +1,14 @@
+import { Box, Image } from '@chakra-ui/react'
+import React from 'react'
+
+const Product = () => {
+  return (
+    <Box>
+        <Box>
+            <Image m="auto" src='https://www.jiomart.com/images/category/2/groceries-20210201.jpeg'/>
+        </Box>
+    </Box>
+  )
+}
+
+export default Product
