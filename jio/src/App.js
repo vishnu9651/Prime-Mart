3f6389293a3418f45296c7addccc0a0f119b1b56
vishnu9651 <vishnukantee0044@gmail.com>
2022-12-15 13:02:11 +0530

    test

--- jio/src/App.js
+++ jio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Slider from "./HomePageVishnu/SliderVishnu/Test";
+
 import MainRoutes from "./Pages/MainRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <MainRoutes />
+  
+
     </div>
   );
 }
