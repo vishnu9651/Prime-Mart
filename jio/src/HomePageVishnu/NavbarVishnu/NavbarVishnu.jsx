cbc562096ca7910d79b2aa0389b592514e0bb72a
vishnu9651 <vishnukantee0044@gmail.com>
2022-12-17 21:53:02 +0530

    nav

---
+++ jio/src/HomePageVishnu/NavbarVishnu/NavbarVishnu.jsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/layout"
+
+const NavbarVishnu=()=>{
+
+
+
+    return (<div>
+<Flex>
+    <Box>icon</Box>
+    <Box>search</Box>
+    <Box>login/signup</Box>
+    <Box>cart icon</Box>
+</Flex>
+    </div>)
+}
+
+export default NavbarVishnu
