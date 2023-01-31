b63c50ec5dfea8e7712aee76b4a7ccc23efecd1e
YelveTejas <tejas.yelve20@gmail.com>
2023-01-31 15:22:06 +0530

    Signup page completed

--- jio/src/HomePageVishnu/NavbarVishnu/NavbarVishnu.jsx
+++ jio/src/HomePageVishnu/NavbarVishnu/NavbarVishnu.jsx
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 import {FaUserAlt} from  "react-icons/fa";
 import { useSelector } from "react-redux";
 const NavbarVishnu=()=>{
-    const isAuth = useSelector((store)=>store.AuthReducer.isAuth)
-    const token = useSelector((store)=>store.AuthReducer.token)
+    // const isAuth = useSelector((store)=>store.AuthReducer.isAuth)
+    // const token = useSelector((store)=>store.AuthReducer.token)
 
     return (<div style={{overflow: "visible"}}>
 <Flex  color="white" fontWeight={"bold"} border={"2px solid #5ce1e6"} padding="5px" backgroundColor={"#5ce1e6"} justifyContent="space-between">
@@ -16,12 +16,9 @@ const NavbarVishnu=()=>{
     <Box marginTop={"15px"}  borderRadius={"7px"} paddingBottom="-5px"><SearchVishnu  /></Box>
     <Box marginTop={"15px"}>
         <Flex   gap={"2rem"}>
-            <Box>{
-                isAuth ?<Link> <Flex  padding={"12px"} > <Box><FaUserAlt size={"25"} color="white" /></Box><Box marginLeft={"5px"}>{token.name}</Box> </Flex></Link>:
-                <Link to="/signup"> <Flex  padding={"12px"} > <Box><FaUserAlt size={"25"} color="white" /></Box> <Box marginLeft={"5px"}>Sign in</Box></Flex></Link>
+            <Box>
                
-               }
-          
+                <Link to="/signup"> <Flex  padding={"12px"} > <Box><FaUserAlt size={"25"} color="white" /></Box> <Box marginLeft={"5px"}>Sign in</Box></Flex></Link>
                 </Box>
                 <Link to="/cart"><Box marginTop="8px" marginLeft={"5px"}><BsCart2 size={30}  /> </Box></Link>
                 <Box marginTop="8px" ><MenuVishnu  /></Box>
@@ -33,3 +30,5 @@ const NavbarVishnu=()=>{
 }
 
 export default NavbarVishnu
+// isAuth ?<Link> <Flex  padding={"12px"} > <Box><FaUserAlt size={"25"} color="white" /></Box><Box marginLeft={"5px"}>{token.name}</Box> </Flex></Link>:
+//                 <Link to="/signup"> <Flex  padding={"12px"} > <Box><FaUserAlt size={"25"} color="white" /></Box> <Box marginLeft={"5px"}>Sign in</Box></Flex></Link>
